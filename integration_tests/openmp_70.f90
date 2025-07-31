d49a21f4a3c3a67c63cf44569f0621206db8d647
Jainan-Tandel <adit4443ya@gmail.com>
2025-07-31 16:18:57 +0000

    target_offload codegen in C

---
+++ integration_tests/openmp_70.f90
@@ -0,0 +1,23 @@
+program openmp_70
+    implicit none
+    real, allocatable, dimension(:) :: a, b
+    integer :: i
+    allocate(a(10000000), b(10000000))
+    
+    b=5
+    
+    !$omp target map(tofrom:a, b)
+        !$omp teams
+            !$omp distribute parallel do
+                do i = 1, 10000000
+                    a(i) = real(i) + b(i)*340
+                end do
+            !$omp end distribute parallel do
+        !$omp end teams
+    !$omp end target
+    
+    print*, a(5), b(5)
+    
+    if(a(5) /= 1705) error stop
+    if(b(5) /= 5) error stop
+end program openmp_70
